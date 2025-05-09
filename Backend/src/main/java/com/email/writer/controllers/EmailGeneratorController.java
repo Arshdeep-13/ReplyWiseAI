dd9e432bee57cc23480d43f68d1ac4afce34d16d
Arshdeep-13 <arshdeeprooprai@gmail.com>
2025-05-09 23:25:37 +0530

    Frontend + Backend + Extension code version 1

---
+++ Backend/src/main/java/com/email/writer/controllers/EmailGeneratorController.java
@@ -0,0 +1,22 @@
+package com.email.writer.controllers;
+
+import com.email.writer.models.EmailRequest;
+import com.email.writer.services.EmailGeneratorService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/email")
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
+public class EmailGeneratorController {
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
